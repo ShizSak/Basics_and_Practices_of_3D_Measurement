6f64732462ecc89dd8b697579ea44d7bdae4fd46
Shizuo Sakamoto <shiz.sakamoto@gmail.com>
2022-06-06 23:52:23 +0900

    Update 3D_measurement.cpp

--- Chapter 2/3D_measurement.cpp
+++ Chapter 2/3D_measurement.cpp
@@ -1,6 +1,6 @@
 // Measure 3D shape by projecting sinusoidal patterns
-// written by Shizuo Sakamoto as example in the book, titled as "Basics and Practices of 3D measurement",
-// to be released by Ohmsha, Ltd.
+// written by Shizuo Sakamoto as example in the book, titled as "Basics and Practices of 3D measurement"
+// released by Ohmsha, Ltd.
 //
 // - version 1.00 on 6th, June, 2022.
 //   developed on Mouse Computer H5-CML, Ubuntu 20.04LTS.
